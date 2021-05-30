8aecd4700a540f6824d7851ccd4083e2e18400ed
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-05-30 15:38:58 +0600

    Install Firebase and Initialize Firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initilalizeApp({
+    apiKey: "AIzaSyBPV8bRy09zTC71AZPNQQosQ_HLNlD8ypU",
+    authDomain: "fir-auth-development-847c3.firebaseapp.com",
+    projectId: "fir-auth-development-847c3",
+    storageBucket: "fir-auth-development-847c3.appspot.com",
+    messagingSenderId: "33848548806",
+    appId: "1:33848548806:web:28c4256a801c7c534be6c7"
+});
+
+const auth = app.auth();
+export { auth };
+export default app;

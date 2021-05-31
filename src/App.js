22e6ebe746caaf30cb6ed5ea272c8deea87127d7
unknown <sarwarsunjid@gmail.com>
2021-06-01 02:40:43 +0600

    Setup Private Route for Secure Login

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Login from './components/Login';
 import UpdateProfile from './components/UpdateProfile';
 import { BrowserRouter, Route } from "react-router-dom";
 import  Dashboard  from './components/Dashboard';
+import PrivateRoute from './PrivateRoute'
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Dashboard} />
+      <PrivateRoute path="/" exact component={Dashboard} />
       <Route path ="/login" exact component={Login} />
       <Route path="/signup" exact component={Signup} />
-      <Route path="/update-profile" exact component={UpdateProfile} />
+      <PrivateRoute path="/update-profile" exact component={UpdateProfile} />
     </BrowserRouter>
   );
 }
